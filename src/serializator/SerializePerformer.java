9023da031194c21e40ce9a2168eeb6903752a7f1
overpathz <overpathz@gmail.com>
2022-01-07 17:18:56 +0200

    add SerializePerformer class and the class with main method to test code

---
+++ src/serializator/SerializePerformer.java
@@ -0,0 +1,11 @@
+package serializator;
+
+public class SerializePerformer {
+    /*
+        Serialization file will be contain:
+        1 line - Class
+     */
+    public void serialize() {
+
+    }
+}
